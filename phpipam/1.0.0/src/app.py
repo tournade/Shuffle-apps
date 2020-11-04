8ceceffb3636360dc270e7361583c900a6642fed
Pascal Johnson <pascal.johnson@kpconcept.com>
2020-11-03 22:13:33 -0500

    modification de la class

--- phpipam/1.0.0/src/app.py
+++ phpipam/1.0.0/src/app.py
@@ -6,9 +6,9 @@ import json
 
 from walkoff_app_sdk.app_base import AppBase
 
-class PythonPlayground(AppBase):
+class PhpIpam(AppBase):
     __version__ = "1.0.0"
-    app_name = "python_playground"  # this needs to match "name" in api.yaml
+    app_name = "PhpIpam"  # this needs to match "name" in api.yaml
 
     def __init__(self, redis, logger, console_logger=None):
         """
@@ -51,23 +51,6 @@ class PythonPlayground(AppBase):
 
             info.append([line['subnet'],str(line['mask']),str(line['vlanId']),str(line['description']),str(location)])
         return info[0][0]
-    # Write your data inside this function
-    async def run_python_script(self, json_data, function_to_execute):
-        # It comes in as a string, so needs to be set to JSON
-        try:
-            json_data = json.loads(json_data)
-        except json.decoder.JSONDecodeError as e:
-            return "Couldn't decode json: %s" % e
-
-        # These are functions
-        switcher = {
-            "function_1" : self.run_me_1,
-            "function_2" : self.run_me_2,
-            "function_3" : self.run_me_3,
-        }
-
-        func = switcher.get(function_to_execute, lambda: "Invalid function")
-        return func(json_data)
 
 if __name__ == "__main__":
     asyncio.run(PythonPlayground.run(), debug=True)
